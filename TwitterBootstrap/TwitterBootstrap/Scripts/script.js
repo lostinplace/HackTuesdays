5057f74fd77017f9ffd3f73987f2c62e20c6d200
Chris Wheeler <cmwhee@gmail.com>
2012-11-20 18:53:17 -0500

    cursors

--- TwitterBootstrap/TwitterBootstrap/Scripts/script.js
+++ TwitterBootstrap/TwitterBootstrap/Scripts/script.js
@@ -1,3 +1,11 @@
 ﻿$(function () {
   $("#txtcmd").focus();
+  var CursorInit = (function () {
+    setInterval(function () {
+      $('.cursor').css('color', '#222');
+      setTimeout(function () {
+        $('.cursor').css('color', '#00ff90');
+      }, 500);
+    }, 1000);
+  })();
 });
