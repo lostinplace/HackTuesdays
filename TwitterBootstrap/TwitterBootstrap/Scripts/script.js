a096dd602adc7797ba2daf98a13e36410f013db8
Ryan <rwestphal@appgeo.com>
2012-11-20 19:06:08 -0500

    woot cursor!

--- TwitterBootstrap/TwitterBootstrap/Scripts/script.js
+++ TwitterBootstrap/TwitterBootstrap/Scripts/script.js
@@ -1,6 +1,10 @@
 ﻿$(function () {
-  $("#txtcmd").val("").focus().on("keypress", function () {
-    $(this).css("width", (($(this).val().length + 2) * .6) + "em");
+  $("#txtcmd").val("").focus().on("keyup", function () {
+    $(".activeline").text($(this).val());
+    //$(this).css("width", (($(this).val().length + 2) * .6) + "em");
+  }).on("blur", function () {
+    var val = $(this).val();
+    $(this).focus().val(val);
   });
   var CursorInit = (function () {
     setInterval(function () {
